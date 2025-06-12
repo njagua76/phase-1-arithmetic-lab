39bcd7d89be45cb7f2bff66c9bef99059dda568f
Ann Gathoni <theanngathoni@gmail.com>
2025-06-12 12:21:23 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100) + 1; // Any random integer > 0
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+const max = Math.max(1, 3, 20, 5, 7);
